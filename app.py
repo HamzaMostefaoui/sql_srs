ffe4cceaed1883d6ec32cc3b22bd4a67a1b21cb5
HamzaMostefaoui <hhamza.mostefaoui@gmail.com>
2024-10-13 16:12:44 +0200

    add ci and cleanup code with black

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-import streamlit as st
-import pandas as pd
-import duckdb
+# pylint: disable=missing-module-docstring
+
 import io
 
-csv = '''
+import duckdb
+import pandas as pd
+import streamlit as st
+
+CSV = """
 Beverage,Price
 Orange juice,2
 Expresso,2
 Tea,3
-'''
+"""
 
-beverages = pd.read_csv(io.StringIO(csv))
+beverages = pd.read_csv(io.StringIO(CSV))
 beverages = pd.DataFrame(beverages)
 
-csv2 = '''
+CSV2 = """
 Food_item,Food_price
 Cookie,2
 Chocolatine,2
 Muffin,3
-'''
+"""
 
-food_items = pd.read_csv(io.StringIO(csv2))
+food_items = pd.read_csv(io.StringIO(CSV2))
 food_items = pd.DataFrame(food_items)
 
 
@@ -32,27 +35,38 @@ with st.sidebar:
         "What would you like to review would you like to review ?",
         ("Joins", "GroupBy", "Window Functions"),
         index=None,
-        placeholder="Select a theme",)
+        placeholder="Select a theme",
+    )
 
     st.write("You selected:", option)
 
-answer = f"""
+ANSWER_STR = """
 SELECT * FROM beverages \n
 CROSS JOIN food_items
 """
 
+solution_df = duckdb.query(ANSWER_STR).df()
+
 
 user_query = st.text_area(label="Enter your query :", placeholder="Your SQL code...")
 if user_query:
-    result = duckdb.query(user_query)
+    result = duckdb.query(user_query).df()
     st.dataframe(result)
 
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
 
-    st.write("Expected result :")
-    solution = duckdb.query(answer).df()
-    st.write(solution)
+    n_lines_differences = result.shape[0] - solution_df.shape[0]
+    if result.shape[0] != solution_df.shape[0]:
+        st.write(
+            f"Your result has {n_lines_differences} lines differences with the solution   "
+        )
 
-tab1,tab2 = st.tabs(["Tables","Solution"])
+
+tab1, tab2 = st.tabs(["Tables", "Solution"])
 
 with tab1:
     st.write("beverages")
@@ -61,8 +75,4 @@ with tab1:
     st.dataframe(food_items)
 
 with tab2:
-    st.write(answer)
-
-
-
-
+    st.write(ANSWER_STR)
