d4ad6ae16dc8749accdbcc7dd96b21d74d716186
HamzaMostefaoui <hhamza.mostefaoui@gmail.com>
2024-10-15 19:45:13 +0200

    add button with spaced repetition system

--- app.py
+++ app.py
@@ -4,83 +4,114 @@ import os
 import logging
 import duckdb
 import streamlit as st
+from datetime import date, timedelta
+
 
 if "data" not in os.listdir():
+    print("creating folder data")
     logging.error(os.listdir())
     logging.error("creating folder data")
     os.mkdir("data")
 
 if "exercises_sql_tables.duckdb" not in os.listdir("data"):
     exec(open("init_db.py").read())
+    # subprocess.run(["python", "init_db.py"])
+
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-
-
-
 st.title("SQL SRS")
-st.header("Spaced Repetion System SQL practice")
+st.header("Spaced Repetion System")
 
 
-
-with st.sidebar:
-    available_themes = con.execute("SELECT theme FROM memory_state").df()
-    available_themes = available_themes.get("theme").unique()
-    theme = st.selectbox(
-        "What would you like to review would you like to review ?",
-        (available_themes),
-        index=None,
-        placeholder="Select a theme",
-    )
-
-    if theme:
-        st.write("You selected:", theme)
-        exercise = (
-            con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'")
-            .df()
-            .sort_values("last_reviewed")
-            .reset_index(drop=True)
-        )
-    else:
-        exercise = (
-            con.execute(f"SELECT * FROM memory_state")
-            .df()
-            .sort_values("last_reviewed")
-            .reset_index(drop=True)
-        )
-
-    exercise_name = exercise.loc[0, "exercise_name"]
-    with open(f"answers/{exercise_name}.sql", "r") as f:
-        answer = f.read()
-    solution_df = con.execute(answer).df()
-
-
-user_query = st.text_area(label="Enter your query :", placeholder="Your SQL code...")
-if user_query:
+def check_users_solution(user_query: str) -> None:
+    """
+    Checks that user SQL query is correct by:
+    1: checking the columns
+    2: checking the values
+    :param user_query: a string containing the query inserted by the user
+    """
     result = con.execute(user_query).df()
     st.dataframe(result)
     try:
         result = result[solution_df.columns]
         st.dataframe(result.compare(solution_df))
+        if result.compare(solution_df).shape == (0, 0):
+            st.write("Correct !")
+            st.balloons()
     except KeyError as e:
         st.write("Some columns are missing")
-
-    n_lines_differences = result.shape[0] - solution_df.shape[0]
-    if result.shape[0] != solution_df.shape[0]:
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
         st.write(
-            f"Your result has {n_lines_differences} lines differences with the solution"
+            f"result has a {n_lines_difference} lines difference with the solution_df"
         )
 
-tab1, tab2 = st.tabs(["Tables", "Solution"])
 
-with tab1:
-    exercises_tables = exercise.loc[0, "tables"]
-    for table in exercises_tables:
-        df_table = con.execute(f"SELECT * FROM {table}")
-        st.dataframe(df_table)
+with st.sidebar:
+    available_themes_df = con.execute("SELECT DISTINCT theme FROM memory_state").df()
+    theme = st.selectbox(
+        "What would you like to review?",
+        available_themes_df["theme"].unique(),
+        index=None,
+        placeholder="Select a theme...",
+    )
+    if theme:
+        st.write(f"You selected {theme}")
+        select_exercise_query = f"SELECT * FROM memory_state WHERE theme = '{theme}'"
+    else:
+        select_exercise_query = f"SELECT * FROM memory_state"
 
-
-with tab2:
+    exercise = (
+        con.execute(select_exercise_query)
+        .df()
+        .sort_values("last_reviewed")
+        .reset_index(drop=True)
+    )
+    # st.write(exercise)
     exercise_name = exercise.loc[0, "exercise_name"]
     with open(f"answers/{exercise_name}.sql", "r") as f:
         answer = f.read()
+
+    solution_df = con.execute(answer).df()
+
+form = st.form("my_form")
+query = form.text_area(label = "Type your code", placeholder="your SQL query...", key="user_input")
+form.form_submit_button("Submit")
+
+with st.sidebar:
+    exercise_tables = exercise.loc[0, "tables"]
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
+
+if query:
+    check_users_solution(query)
+
+
+cols = st.columns(3)
+for i, n_days in enumerate([2, 7, 21]):
+    with cols[i]:  # Place each button in its respective column
+        if st.button(f"See again in {n_days} days"):
+            next_review = date.today() + timedelta(days=n_days)
+            con.execute(
+                "UPDATE memory_state SET last_reviewed = ? WHERE exercise_name = ?",
+                (next_review, exercise_name)
+            )
+            st.rerun()
+
+if st.button("Reset"):
+    con.execute(f"UPDATE memory_state SET last_reviewed = '1970-01-01'")
+    st.rerun()
+
+tab2, tab3 = st.tabs(["Expected result", "Solution"])
+with tab2:
+    solution_df
+
+with tab3:
     st.write(answer)
+
+
+
+
+
